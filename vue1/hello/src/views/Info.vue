83f233015910e6310100130e0bc2f65117a7d4e0
xuyunxin <190514284@qq.com>
2021-07-19 17:14:14 +0800

    3

--- vue1/hello/src/views/Info.vue
+++ vue1/hello/src/views/Info.vue
@@ -1,13 +1,21 @@
 <template>
-
+  <div class="title">
+    <ul>
+      <li class="item">文章1</li>
+      <li class="item">文章2</li>
+      <li class="item">文章3</li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "Info"
+  name: 'Info'
 }
 </script>
 
 <style scoped>
-
+.title{
+  background: red;
+}
 </style>
